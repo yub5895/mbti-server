ad0e2ae168f6f64bf6d56627cec0e09dc6f446ea
BAEYEONGUN <yub589572@naver.com>
2024-10-07 17:40:52 +0900

    Initial commit

---
+++ src/test/java/com/server/mbti/MbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.mbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
